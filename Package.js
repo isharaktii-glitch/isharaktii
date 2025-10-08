3235e78142a7f1cc17ae56dbf1619487abd053ca
Iahara <isharaktii@gmail.com>
2025-10-08 13:11:42 +0530

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,14 @@
+{
+  "name": "video-streaming-site",
+  "version": "1.0.0",
+  "dependencies": {
+    "express": "^4.17.1",
+    "mongoose": "^6.0.0",
+    "multer": "^1.4.3",
+    "cloudinary": "^1.26.0",
+    "tesseract.js": "^2.1.1"
+  },
+  "scripts": {
+    "start": "node server/server.js"
+  }
+}
